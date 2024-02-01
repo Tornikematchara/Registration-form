3da1e61181ebea6980040f3e18ceb9ee960fd7e2
Tornikematchara <tornikematcharashvili10@gmail.com>
2024-02-01 18:35:42 +0400

    first commit

---
+++ check.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<h1>Member Status</h1>
+
+<?php
+    if ($_SERVER ["REQUEST_METHOD"] == "POST") {
+        //COLLECT INPUT DATA
+        $username = $_POST ["username"];
+        $password = $_POST ["password"];
+        $confirmPassword = $_POST ["confirmPassword"];
+
+        //check if password match
+        if ($password == $confirmPassword) {
+            echo "Registration successful.<br>";
+            echo $username; 
+         } else { 
+            echo "Password do not match. Try again.";
+         }
+    }
+
+
+?>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="check.php" method="post">
+        <div>
+            <label for="username">სახელი:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div>
+            <label for="password">პაროლი:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div>
+            <label for="confirmPassword">გაიმეორეთ პაროლი:</label>
+            <input type="password" id="confirmPassword" name="confirmPassword" required>
+        </div>
+        <div>
+            <button type="submit">Register</button>
+        </div>
+
+
+
+
+    </form>
+    
+
+
+
+
+</body>
+</html>
